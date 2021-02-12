5f712a034dd27aa92fe29e2f99947848b884851b
anandukdev <anandukdev@gmail.com>
2021-02-12 11:49:09 +0530

    App

---
+++ APP/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="app">
+
+<Navbar></Navbar>
+
+<Table></Table>
+
+<Search></Search>
+
+<!-- <Button></Button> -->
+
+<!-- <Pagination></Pagination> -->
+
+<!-- <Getdata></Getdata> -->
+
+</div>
+</template>
+
+<script>
+import Navbar from './components/Navbar'
+import Table from './components/Table'
+// import Button from './components/Button'
+// import Search from './components/Search'
+
+export default {
+  components: {
+    Navbar, Table 
+    } , 
+  data(){
+    return{
+
+    }
+  }
+  
+}
+</script>
+
+
+
+
+<style scoped>
+
+</style>
